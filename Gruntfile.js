df267ef4c99684087822c17c68e48ae249ea1744
Leonardo Massafelli <leonardo.melo@webjump.com.br>
2022-01-14 11:16:47 -0300

    Subindo arquivos do curso finalizado

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
-module.exports = function(grunt) {
-  require('jit-grunt')(grunt);
+module.exports = function (grunt) {
+  require("jit-grunt")(grunt);
 
   grunt.initConfig({
     less: {
       development: {
         files: {
           "css/estilos.css": "css/estilos.less", // Caminho dos arquivos
-        }
-      }
+        },
+      },
     },
     watch: {
       styles: {
-        files: ['**/*.less'], // Quais arquivos o grunt ficará de olho
-        tasks: ['less']
-      }
-    }
+        files: ["**/*.less"], // Quais arquivos o grunt ficará de olho
+        tasks: ["less"],
+      },
+    },
   });
 
-  grunt.registerTask('default', ['less', 'watch']);
+  grunt.registerTask("default", ["less", "watch"]);
 };
